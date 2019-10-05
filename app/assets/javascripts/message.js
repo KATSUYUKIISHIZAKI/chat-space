44df9c52fdc5e4d23ba679d05881eda3c521558a
zaki7070 <active.enjoy.life2000@gmail.com>
2019-10-05 21:11:43 +0900

    delete console

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -24,7 +24,7 @@ $(function(){
     e.preventDefault();
     var formData = new FormData(this);
     var url = $(this).attr('action');
-    console.log(url);
+    
     $.ajax({
       url: url,
       type: "POST",
