2d2ed50399498017a030924a352d6128024b4aa1
zaki7070 <active.enjoy.life2000@gmail.com>
2019-10-07 21:59:50 +0900

    Revert "first commit"
    This reverts commit 9f28415f44ce972c198fef0bf4a078c1c2414b29.

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
+  # root to: 'messages#index'
   root 'groups#index'
   resources :users, only: [:edit, :update]
   resources :groups, only: [:new, :create, :edit, :update] do
     resources :messages, only: [:index, :create]
   end
-<<<<<<< HEAD
 
-=======
->>>>>>> parent of b7a2af7... Merge pull request #5 from KATSUYUKIISHIZAKI/Asynchronous-message-sending-function
 end
