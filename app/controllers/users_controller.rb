d6f9cfc8bb12765ac9e9cc65e0ecf26da0e0140a
zaki7070 <active.enjoy.life2000@gmail.com>
2019-10-17 20:21:52 +0900

    app-controllers-customaize

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,8 +9,9 @@ class UsersController < ApplicationController
   end
 
   def edit
-
+    
   end
+
   def update
     if current_user.update(user_params)
       redirect_to root_path
@@ -19,7 +20,9 @@ class UsersController < ApplicationController
     end
   end
 end
+
 private
+
 def user_params
   params.require(:user).permit(:name, :email)
 end
