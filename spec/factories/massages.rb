bb6b6a9a546fcc40a740aaa7bf0897d698bd3707
zaki7070 <active.enjoy.life2000@gmail.com>
2019-10-18 22:28:37 +0900

    spec-update

--- spec/factories/massages.rb
+++ spec/factories/massages.rb
@@ -1,8 +1,11 @@
-FactoryBot.define do
+FactoryGirl.define do
+
   factory :message do
-    content {Faker::Lorem.sentence}
-    image {File.open("#{Rails.root}/public/images/test_image.jpg")}
+    body  Faker::Lorem.sentence
+    image Rack::Test::UploadedFile.new(File.join(Rails.root, 'spec/factories/fixtures/icon.jpg'))
+    created_at { Faker::Time.between(2.days.ago, Time.now, :all) }
     user
     group
   end
-end 
+
+end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -6,7 +6,8 @@ require File.expand_path('../../config/environment', __FILE__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
-
+require 'devise'
+require File.expand_path("spec/support/controller_macros.rb")
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
@@ -31,10 +32,6 @@ rescue ActiveRecord::PendingMigrationError => e
   exit 1
 end
 RSpec.configure do |config|
-  Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
-  config.include Devise::Test::ControllerHelpers, type: :controller
-  config.include ControllerMacros, type: :controller
-  config.include FactoryBot::Syntax::Methods
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
@@ -62,4 +59,7 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+  config.include FactoryGirl::Syntax::Methods
+  config.include Devise::TestHelpers, type: :controller
+  config.include ControllerMacros, type: :controller
 end
