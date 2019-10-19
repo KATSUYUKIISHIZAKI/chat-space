5f9deb2eb6a8bea424732c4668cc000d2f0f3e20
zaki7070 <active.enjoy.life2000@gmail.com>
2019-10-19 20:04:13 +0900

    spec-test-factbot-update02

--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -1,86 +1,92 @@
 require 'rails_helper'
 
-RSpec.describe MessagesController, type: :controller do
+describe MessagesController do
   let(:group) { create(:group) }
   let(:user) { create(:user) }
 
-  describe "get#index" do
-    context "log in" do
+  describe '#index' do
+
+    context 'log in' do
       before do
-        login_user(user)
-        get :index, params: { group_id: group }
+        login user
+        get :index, params: { group_id: group.id }
       end
 
-      it "assign @message" do
+      it 'assigns @message' do
         expect(assigns(:message)).to be_a_new(Message)
       end
 
-      it "assign @group" do
+      it 'assigns @group' do
         expect(assigns(:group)).to eq group
       end
 
-      it "renders the :index template" do
+      it 'redners index' do
         expect(response).to render_template :index
       end
     end
 
-    context "not log in" do
+    context 'not log in' do
       before do
-        get :index, params: { group_id: group }
+        get :index, params: { group_id: group.id }
       end
 
-      it "redirects to new_user_settion_path" do
+      it 'redirects to new_user_session_path' do
         expect(response).to redirect_to(new_user_session_path)
       end
     end
   end
 
-    describe "post#crate" do
-    let(:params) { { group_id: group.id, user_id: user.id, message: attributes_for(:message) } }
 
-    context "log in" do
+  describe '#create' do
+    let(:params){{group_id: group.id, user_id: user.id, message: attributes_for(:message)}}
+
+    context 'log in' do
       before do
-        login_user(user)
+        login user
       end
 
-      context "can save" do
-        subject {
-          post :create, params: params
+      context 'can save' do
+        subject{
+          post :create,
+          params: params
         }
 
-        it 'count up message' do
-          expect{ subject }.to change(Message, :count).by(1)
-        end
+        it "count up message" do
+          expect{subject}.to change(Message, :count).by(1)
+      end
 
-        it 'redirects to group_messages_path' do
+        it "redirect to group_messages_path" do
           subject
           expect(response).to redirect_to(group_messages_path(group))
         end
       end
 
-      context 'can not save' do
-        let(:invalid_params) { { group_id: group.id, user_id: user.id, message: attributes_for(:message, body: nil, image: nil) } }
+      context 'can not save' do 
+        let(:invalid_params){{group_id: group.id, user_id: user.id, message: attributes_for(:message, content: nil, image: nil)}}
 
-        subject {
-          post :create, params: invalid_params
-        }
+          subject{
+            post :create,
+            params: invalid_params
+          }
 
-        it 'does not count up' do
-          expect{ subject }.not_to change(Message, :count)
-        end
+          it 'does not count up' do
+            expect{subject}.not_to change(Message, :count)
+          end
 
-        it 'renders index' do
-          subject
-          expect(response).to render_template :index
-        end
+          it 'renders index' do
+            subject
+            expect(response).to render_template :index
+          end
       end
     end
 
-    context "not log in" do
-      it 'redirects to new_user_session_path' do
+    context 'not log in' do
+
+      it 'redirect to new_user_session_path' do
         post :create, params: params
         expect(response).to redirect_to(new_user_session_path)
       end
+
     end
   end
 end
