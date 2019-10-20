878d960ea68c57c068d00a676450fe15ee6b768c
zaki7070 <active.enjoy.life2000@gmail.com>
2019-10-20 12:44:48 +0900

    update-20191020

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,70 +1,74 @@
-$(document).on('turbolinks:load', function(){
+$(document).on('turbolinks:load', function(){  
+  function buildHTML(message){
 
-  var search_list = $("#user-search-result");
-  var memberslist = $("#member-search-result");
-  
-  function appendUser(user){
-    var html = `<div class="chat-group-user clearfix">
-                  <p class="chat-group-user__name">${user.name}</p>
-                  <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</div>
+    var image = message.image.url ? `<img class="message__image" src="${message.image.url}">`: "";
+
+    var html = `<div class="message" data-message-id= "${message.id}">
+                  <div class="message__user">
+                    ${message.user_name}
+                  <div class="message__user__date"></div>
+                    ${message.date}
+                  </div>
+                  <div class="message__text">
+                  <p class="message__content">
+                    ${message.content}
+                  </p>
+                  ${image}
+                  </div>
                 </div>`
-    
-    search_list.append(html);
+    return html;
   }
-  
-  function appendErrMsgToHTML(word){
-    var html = `<div class="chat-group-user clearfix">
-                  <p class="chat-group-user__name">${word}</p>
-                </div>`
-    search_list.append(html);
-  }
-  
-  function appendMembers(name, user_id){
-    var html = `<div class='chat-group-user clearfix'>
-                  <input name='group[user_ids][]' type='hidden' value='${user_id}'>
-                  <p class='chat-group-user__name'>${name}</p>
-                  <div class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</div>
-                </div>`
-    memberslist.append(html);
-  }
-  
-    $("#user-search-field").on("keyup", function(){
-      var input = $("#user-search-field").val();
-  
-      $.ajax({
-        type: 'GET',
-        url: '/users',
-        data: {keyword: input},
-        dataType: 'json'
+
+  $('#new_message').on('submit', function(e){
+  e.preventDefault();
+    var formData = new FormData(this);
+    var url = (window.location.href);
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+
+    .done(function(data){
+      $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
+      var html = buildHTML(data);
+      $('.messages').append(html);
+      $('form')[0].reset();
+  })
+
+    .fail(function(data){
+    alert('メッセージを送信できません');
       })
-  
-      .done(function(users){
-        $("#user-search-result").empty();
-        if(users.length !== 0){
-          users.forEach(function(user){
-            appendUser(user);
-          });
-        }
-        else {
-          appendErrMsgToHTML("一致するユーザーが見つかりません");
-        }
-      })
-      .fail(function(){
-        alert('ユーザー検索に失敗しました')
-      })
-    });
-  
-    $(function(){
-      $(document).on('click', '.user-search-add', function(){
-        var name = $(this).data("user-name");
-        var user_id = $(this).data("user-id");
-        $(this).parent().remove();
-        appendMembers(name, user_id);
-      });
-  
-      $(document).on('click', '.user-search-remove', function(){
-        $(this).parent().remove();
-      })
-  
-    });
+
+    .always(function(data){
+    $('.send-btn').prop('disabled', false);
+    })
   });
+
+  var reloadMessages = function(){
+    if (window.location.href.match(/\/groups\/\d+\/messages/)){
+    var last_message_id = $('.message').last().data("message-id");
+    $.ajax({
+      url: "api/messages",
+      type: 'GET',
+      dataType: 'json',
+      data: {id: last_message_id}
+    })
+    .done(function(messages){
+      var insertHTML = ''; //追加するHTMLの入れ物を作る
+      messages.forEach(function(message){  //配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
+        insertHTML = buildHTML(message); //メッセージが入ったHTMLを取得
+        $('.messages').append(insertHTML); //メッセージを追加
+      })
+      $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
+    })
+    .fail(function(){
+      alert('error');
+      });
+    }
+  };
+    setInterval(reloadMessages, 5000);
+});
