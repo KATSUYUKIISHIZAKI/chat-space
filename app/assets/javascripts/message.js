72371658b4d88607dd9216236f36ac2bf80bb5fb
zaki7070 <active.enjoy.life2000@gmail.com>
2019-10-20 23:09:25 +0900

    app-asset-view-01-update20191020

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,28 +1,25 @@
-$(document).on('turbolinks:load', function(){  
+$(document).on('turbolinks:load', function() {
   function buildHTML(message){
-
-    var image = message.image.url ? `<img class="message__image" src="${message.image.url}">`: "";
-
-    var html = `<div class="message" data-message-id= "${message.id}">
-                  <div class="message__user">
+    var image = message.image ? `<img src="${message.image}">` : ``
+    var html = `<ul class="main-block__user-name">
                     ${message.user_name}
-                  <div class="message__user__date"></div>
-                    ${message.date}
-                  </div>
-                  <div class="message__text">
-                  <p class="message__content">
-                    ${message.content}
-                  </p>
-                  ${image}
-                  </div>
-                </div>`
+                    <li class="main-block__user-name__time">
+                      ${message.created_at}
+                    </li>
+                  </ul>
+                  <div class="main-block__comments">
+                    <p>
+                      ${message.content}
+                    </p>
+                    ${image}
+                  </div>`
     return html;
   }
 
-  $('#new_message').on('submit', function(e){
-  e.preventDefault();
+  $('.new_message').on('submit', function(e){
+    e.preventDefault();
     var formData = new FormData(this);
-    var url = (window.location.href);
+    var url = $(this).attr('action')
     $.ajax({
       url: url,
       type: "POST",
@@ -31,44 +28,42 @@ $(document).on('turbolinks:load', function(){
       processData: false,
       contentType: false
     })
-
     .done(function(data){
-      $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
       var html = buildHTML(data);
-      $('.messages').append(html);
-      $('form')[0].reset();
-  })
-
-    .fail(function(data){
-    alert('メッセージを送信できません');
-      })
-
-    .always(function(data){
-    $('.send-btn').prop('disabled', false);
-    })
-  });
-
-  var reloadMessages = function(){
-    if (window.location.href.match(/\/groups\/\d+\/messages/)){
-    var last_message_id = $('.message').last().data("message-id");
-    $.ajax({
-      url: "api/messages",
-      type: 'GET',
-      dataType: 'json',
-      data: {id: last_message_id}
-    })
-    .done(function(messages){
-      var insertHTML = ''; //追加するHTMLの入れ物を作る
-      messages.forEach(function(message){  //配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
-        insertHTML = buildHTML(message); //メッセージが入ったHTMLを取得
-        $('.messages').append(insertHTML); //メッセージを追加
-      })
-      $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
+      $(".main").append(html)
+      $(".footer__framwork__input-box__form-message").val('')
+      $(".footer__framwork__form-submit").prop('disabled', false);
     })
     .fail(function(){
-      alert('error');
-      });
-    }
+      alert('失敗しました。');
+    })
+  })
+
+  $('document').ready(function(){
+    $(".main").animate({scrollTop:$('.footer__framwork')}, 500, 'swing');
+  })
+
+  var reloadMessages = function(message) {
+    last_message_id = $("#blog")
+    group_id = $(".main-contents__header__block__group-name").data('group_id')
+    var url = `/groups/${group_id}/api/messages`
+    $.ajax({
+      url: url,
+      type: 'get',
+      dataType: 'json',
+      data: {id: last_message_id},
+      processData: false,
+      contentType: false
+    })
+    .done(function(messages) {
+      var insertHTML = '';
+      messages.forEach(function(message){
+        insertHTML += buildHTML(message);
+      })
+      $('.main').append(insertHTML)
+    })
+    .fail(function() {
+    });
   };
-    setInterval(reloadMessages, 5000);
+  setInterval(reloadMessages, 3000);
 });
